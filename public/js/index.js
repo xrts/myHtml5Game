8f537fd0aeec52eb3cf34675e20c28c3ffe3d7f3
chengji <286316299@qq.com>
2018-03-16 17:18:46 +0800

    修改

---
+++ public/js/index.js
@@ -0,0 +1,15 @@
+(function(){
+    var startBtn = $("#start");
+    var start = $(".start");
+    var main = $(".main");
+    var ul = $(".options");
+    //点击开始按钮切换到答题页面
+    startBtn.click(function(){
+
+        start.css('display','none');
+        main.css('display','block');
+    })
+    //答题效果
+
+
+})()
